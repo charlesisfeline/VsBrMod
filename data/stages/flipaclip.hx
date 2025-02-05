4dbee7f702182cba46b48fcf9273540b49519f69
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-02-04 19:47:53 -0500

    3 2 1 br

--- data/stages/flipaclip.hx
+++ data/stages/flipaclip.hx
@@ -4,7 +4,9 @@ function postCreate()
 {
 	FlxG.camera.bgColor = 0xFFFFFFFF;
 
-	flipaclippin = new FlxSprite().loadGraphic(Paths.image('stages/flipaclip/flipalogo'));
+	flipaclippin = new FlxSprite(15, 635).loadGraphic(Paths.image('stages/flipaclip/flipalogo'));
+	flipaclippin.setGraphicSize(Std.int(flipaclippin.width * 0.25));
+	flipaclippin.updateHitbox();
 	add(flipaclippin);
 
 	flipaclippin.cameras = [camHUD];
