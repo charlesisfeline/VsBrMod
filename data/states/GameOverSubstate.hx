b6d7eb850f6c2a66d9ab22b3a72f4c758b4b85ab
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-02-05 15:58:10 -0500

    Update GameOverSubstate.hx

--- data/states/GameOverSubstate.hx
+++ data/states/GameOverSubstate.hx
@@ -8,7 +8,6 @@ if (PlayState.SONG.meta.name != "depart")
 	video.onEndReached.add(function():Void
 	{
 		video.dispose();
-
 		FlxG.removeChild(video);
 	});
 	FlxG.addChildBelowMouse(video);
@@ -19,25 +18,23 @@ if (PlayState.SONG.meta.name != "depart")
 		lossSFX.volume = 0;
 		if (FlxG.sound.music != null)
 		{
+			FlxG.sound.music.volume = 0;
 			FlxG.sound.music.stop();
 		}
-
 		if (controls.BACK || controls.ACCEPT)
 		{
 			FlxG.game.removeChild(video);
+			FlxG.sound.music.volume = 1;
 			FlxG.switchState(new PlayState());
 		}
-
 		if (video.time == -1)
 		{
 			video.onEndReached.add(function():Void
 			{
 				video.dispose();
-
 				FlxG.removeChild(video);
 			});
 			FlxG.addChildBelowMouse(video);
-
 			if (video.load(Paths.video("retry")))
 				new FlxTimer().start(0.001, (_) -> video.play());
 		}
