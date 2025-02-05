393a36c83c5da120c404ee94f06a414552b4a1d7
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-02-05 15:44:12 -0500

    temporary rt clock
    i chart/code late at evening or night so just incase i gotta go, i just put this on to keep track of da time and point to it, nonchalantly ...
    will be removed when we release the mod

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -1,4 +1,7 @@
+import Date;
+
 var icoPlacement:String = "left";
+var timeFormat12h:Bool = true; //the illusion of choice
 
 function postCreate() {
     scoreText.alpha = 1;
@@ -7,7 +10,36 @@ function postCreate() {
     scoreBG.alpha = 0.6;
 
     diffText.destroy();
+
+timeTxt = new FunkinText(0, FlxG.height - 60, 400, "X:XX", 32, true);
+    timeTxt.borderSize = 2;
+    timeTxt.alignment = "center";
+    timeTxt.scrollFactor.set();
+    timeTxt.updateHitbox();
+    add(timeTxt);
+
+    timeBG = new FlxSprite(timeTxt.x + 80,timeTxt.y - 15).makeGraphic(timeTxt.width - 160,timeTxt.height + 40,FlxColor.GRAY);
+    timeBG.scrollFactor.set();
+    insert(members.indexOf(timeTxt),timeBG);
 }
+
+function update(elapsed:Float) {
+    var theDate = Date.now();
+	var hours = theDate.getHours();
+    var minutes = theDate.getMinutes();
+    var seconds = theDate.getSeconds();
+
+    if (timeFormat12h){
+        //24 hr system haters gonna cry
+        if (hours >= 12) {			
+            hours -= 12;
+        }
+    }
+
+	var formattedTime = (hours < 10 ? "0" : "" ) + hours + ":" + (minutes < 10 ? "0" : "" ) + minutes + ":" + (seconds < 10 ? "0" : "" ) + seconds + " " + (timeFormat12h ? (hours >= 12 ? "AM" : "PM") : "" );
+	timeTxt.text = formattedTime;
+}
+
 
 function postUpdate() {
     for (p in 0...iconArray.length) {
