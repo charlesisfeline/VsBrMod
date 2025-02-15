6678d8eb28118803dbdb7490afd0aa6820d77be5
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-02-14 23:26:48 -0500

    fill

---
+++ songs/feeling b/scripts/forceBar.hx
@@ -0,0 +1,16 @@
+import funkin.options.Options;
+
+var ogSet:Bool;
+
+function create()
+{
+    ogSet = Options.colorHealthBar;
+    trace(ogSet);
+    Options.colorHealthBar = true;
+}
+
+function destroy()
+{
+    trace(ogSet);
+    Options.colorHealthBar = ogSet;
+}
