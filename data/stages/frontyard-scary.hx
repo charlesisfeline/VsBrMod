9f1babcd4368177c2a50e6ecf20a68e52cb008c8
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-03-28 00:00:15 -0400

    window popups

--- data/stages/frontyard-scary.hx
+++ data/stages/frontyard-scary.hx
@@ -1,3 +1,9 @@
+var windows:FlxSpriteGroup;
+
+// this is mostly just rb's stage code but edited a bit to act like fake window popups. 
+// u cant drag them tho. for now....
+// WIP! 
+
 var borders:FlxSprite;
 
 function postCreate() {
@@ -7,4 +13,28 @@ function postCreate() {
     add(borders);
     
     borders.cameras = [camHUD];
+
+    windows = new FlxSpriteGroup();
+    insert(7, windows); // wishing cne will add `zIndex` like v-slice.....
+    windows.cameras = [camHUD];
+}
+
+
+function addWindow(popup:Int = 2)
+{
+        var spr:FlxSprite = new FlxSprite(0, 0);
+        spr.frames = Paths.getSparrowAtlas("stages/house-scary/windowPopups");
+        spr.x = FlxG.random.int(0, Std.int(FlxG.width - spr.width));
+        spr.y = FlxG.random.int(0, Std.int(FlxG.height - spr.height));
+        for (pop in 0...9) 
+             spr.animation.addByPrefix("window" + pop, "window" + pop, 24, true);
+        spr.updateHitbox();
+        spr.antialiasing = true;
+        spr.playAnim("window" + popup);
+        windows.add(spr);
+}
+
+function update(elapsed:Float)
+{
+// not now bud
 }
