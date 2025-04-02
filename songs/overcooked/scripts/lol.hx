f11bb50ab102a3b226657ebbf8bc7b324300c6ad
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-02 18:44:41 -0400

    Update lol.hx

--- songs/overcooked/scripts/lol.hx
+++ songs/overcooked/scripts/lol.hx
@@ -1,4 +1,10 @@
+var drain:Bool = true;
+
 function onDadHit(NoteHitEvent)
-{
-    if (!player.cpu && health > 0.1) health = health - 0.01;
+    if (drain && !player.cpu && health > 0.1) health = health - 0.01;
+    
+function beatHit(curBeat:Int) {
+    if (curBeat >= 680) drain = false;
+    else
+        drain = true;
 }
