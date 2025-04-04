16458f36bfcac0dcd5f83e38cb092ae1d166fd75
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-04 18:02:04 -0400

    warning yall for spamming the eg week 10 times when u dont have him unlocked

---
+++ data/states/CanYouStop.hx
@@ -0,0 +1,76 @@
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.sound.FlxSound;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+
+import funkin.backend.FunkinText;
+import funkin.backend.utils.FunkinParentDisabler;
+import funkin.menus.StoryMenuState;
+
+var parentDisabler:FunkinParentDisabler;
+var theCam:FlxCamera;
+var bg:FlxSprite;
+var bro:FlxSprite;
+var versionShit:FlxText;
+
+function create() {
+    add(parentDisabler = new FunkinParentDisabler());
+    
+    for (camera in FlxG.cameras.list)
+        camera.active = false;
+        
+    trace("dies");
+    
+    StoryMenuState.canSelect = false;
+    
+    theCam = new FlxCamera();
+    theCam.bgColor = 0;
+    FlxG.cameras.add(theCam, false);
+    cameras = [theCam];
+    
+    bg = new FlxSprite().makeSolid(FlxG.width + 100, FlxG.height + 100, FlxColor.BLACK);
+    bg.updateHitbox();
+    bg.alpha = 0;
+    bg.screenCenter();
+    bg.scrollFactor.set();
+    add(bg);
+    
+    bro = new FlxSprite(0, 0).loadGraphic(Paths.image('menus/storymenu/doitFirst'));
+    bro.scale.set(0.5, 0.5);
+    bro.alpha = 0;
+    bro.scrollFactor.set();
+    bro.antialiasing = Options.antialiasing;
+    bro.updateHitbox();
+    bro.screenCenter();
+    add(bro);
+    
+    versionShit = new FunkinText(0, FlxG.height, 0, "just press B to close this idk", 14);
+    versionShit.scrollFactor.set();
+    versionShit.screenCenter(FlxAxes.X);
+    add(versionShit);
+    
+    FlxG.sound.play(Paths.sound("menu/error"));
+    
+    FlxTween.tween(bg, {alpha: 0.6}, 0.4, {ease: FlxEase.cubeOut});
+    FlxTween.tween(bro, {alpha: 1}, 0.4, {ease: FlxEase.cubeOut});
+    FlxTween.tween(versionShit, {y: FlxG.height - 18}, 0.75, {ease: FlxEase.quadOut});
+    FlxTween.tween(FlxG.sound.music, {pitch: FlxG.random.float(0.3, 0.6)}, 1, {ease: FlxEase.quadInOut});
+}
+
+function update(elapsed:Float) {
+    if (FlxG.keys.justPressed.B) {
+        StoryMenuState.canSelect = true;
+        FlxTween.tween(FlxG.sound.music, {pitch: 1}, 1, {ease: FlxEase.quadOut});
+        FlxG.sound.play(Paths.sound("menu/cancel"));
+        close();
+    }
+}
+
+function destroy() {
+    if (FlxG.cameras.list.contains(theCam)) FlxG.cameras.remove(theCam, true);
+    
+    for (camera in FlxG.cameras.list)
+        camera.active = true;
+}
--- data/states/StoryMenuState.hx
+++ data/states/StoryMenuState.hx
@@ -1,10 +1,13 @@
-import funkin.editors.ui.UIWarningSubstate;
-
 import flixel.util.FlxTimer;
 
 import funkin.savedata.FunkinSave;
 
+var spamEgTimes:Int = 0;
+
 function postCreate() {
+    persistentUpdate = true;
+    persistentDraw = true;
+    
     if (FlxG.save.data.egUnlocked == null) FlxG.save.data.egUnlocked = false;
     trace(FlxG.save.data.egUnlocked);
     
@@ -25,11 +28,22 @@ function onWeekSelect(event) {
     trace(FlxG.save.data.egUnlocked);
     
     if (curWeek != 0 && !FlxG.save.data.egUnlocked) {
-        trace("locked");
-        FlxG.sound.play(Paths.sound("menu/warningMenu"));
-        FlxG.camera.shake(0.01, 0.1);
+        if (spamEgTimes >= 9) {
+            trace("u spammed like " + spamEgTimes + " times wtf???");
+            persistentUpdate = false;
+            persistentDraw = true;
+            spamEgTimes = 0;
+            openSubState(new ModSubState("CanYouStop"));
+        }
+        else {
+            spamEgTimes += 1;
+            trace("locked " + spamEgTimes);
+            FlxG.sound.play(Paths.sound("menu/warningMenu"));
+            FlxG.camera.shake(0.025, 0.1);
+        }
     }
     else {
+        spamEgTimes = 0;
         canSelect = false;
         trace("les go");
         FlxG.sound.play(Paths.sound("menu/confirm"));
