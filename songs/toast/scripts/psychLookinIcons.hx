94c1569960780dd34fab0ca695f68e0e15a7a014
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-10 19:42:01 -0400

    Update psychLookinIcons.hx

--- songs/toast/scripts/psychLookinIcons.hx
+++ songs/toast/scripts/psychLookinIcons.hx
@@ -1,41 +1,15 @@
-public var icoP1:HealthIcon;
-public var icoP2:HealthIcon;
-
 function postCreate() {
-    icoP1 = new HealthIcon(boyfriend != null ? boyfriend.getIcon() : "face", true);
-    icoP2 = new HealthIcon(dad != null ? dad.getIcon() : "face", false);
-    
-    for (ico in [icoP1, icoP2]) {
-        ico.y = healthBar.y - (ico.height / 2);
-        ico.cameras = [camHUD];
-        insert(members.indexOf(healthBar) + 2, ico);
-    }
-    
-    for (i in [iconP1, iconP2])
-        remove(i); // fuck you og icons
+    doIconBop = false;
+    iconP1.origin.set(0, iconP1.height / 2);
+    iconP2.origin.set(iconP2.width, iconP2.height / 2);
 }
 
-function update(elapsed:Float) {
-    icoP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0)) - 26);
-    icoP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0))) - (icoP2.width - 26);
-    
-    updateIconsScale(elapsed);
-    
-    icoP1.health = healthBar.percent / 100;
-    icoP2.health = 1 - (healthBar.percent / 100);
+function postUpdate(e) {
+    iconP1.scale.set(lerp(iconP1.scale.x, 1, 0.15), lerp(iconP1.scale.y, 1, 0.15));
+    iconP2.scale.set(lerp(iconP2.scale.x, 1, 0.15), lerp(iconP2.scale.y, 1, 0.15));
 }
 
-function beatHit() {
-    for (i in [icoP1, icoP2]) {
-        i.scale.set(1.2, 1.2);
-        i.updateHitbox();
-    }
-}
-
-public function updateIconsScale(elapsed:Float) {
-    for (i in [icoP1, icoP2]) {
-        var mult:Float = FlxMath.lerp(1, iconP1.scale.x, Math.exp(-elapsed * 9));
-        i.scale.set(mult, mult);
-        i.updateHitbox();
-    }
+function beatHit(curBeat) {
+    iconP1.scale.set(1.15, 1.15);
+    iconP2.scale.set(1.15, 1.15);
 }
