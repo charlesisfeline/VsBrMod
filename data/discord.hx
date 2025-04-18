61b671a48c75b7d699ea4b017f02f07211e51154
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-18 12:42:35 -0400

    Update discord.hx

--- data/discord.hx
+++ data/discord.hx
@@ -1,43 +1,34 @@
 import funkin.backend.utils.DiscordUtil;
 
-function onGameOver()
-{
+function onGameOver() {
     DiscordUtil.changePresence('get breaded', "NO LEAKS");
-    // DiscordUtil.changePresence('get breaded', PlayState.SONG.meta.displayName + " (" + PlayState.difficulty + ")");
+    // DiscordUtil.changePresence('get breaded', PlayState.SONG.meta.displayName);
 }
 
-function onDiscordPresenceUpdate(e)
-{
+function onDiscordPresenceUpdate(e) {
     var data = e.presence;
     
     if (data.button1Label == null) data.button1Label = "Vs. br Discord";
     if (data.button1Url == null) data.button1Url = "https://discord.gg/8C8p2jsqK9";
 }
 
-function onPlayStateUpdate()
-{
+function onPlayStateUpdate() {
     DiscordUtil.changeSongPresence(PlayState.instance.detailsText, (PlayState.instance.paused ? "PAUSED GAME - " : "") + "NO LEAKS", PlayState.instance.inst,
         PlayState.instance.getIconRPC());
     /* DiscordUtil.changeSongPresence(PlayState.instance.detailsText,
         (PlayState.instance.paused ? "PAUSED GAME - " : "")
-        + PlayState.SONG.meta.displayName
-        + " ("
-        + PlayState.difficulty
-        + ")", PlayState.instance.inst,
+        + PlayState.SONG.meta.displayName, PlayState.instance.inst,
         PlayState.instance.getIconRPC());
      */
 }
 
-function onMenuLoaded(name:String)
-{
+function onMenuLoaded(name:String) {
     // Name is either "Main Menu", "Freeplay", "Title Screen", "Options Menu", "Credits Menu", "Beta Warning", "Update Available Screen", "Update Screen"
     DiscordUtil.changePresenceSince("we scrolling menus with this one", null);
 }
 
-function onEditorTreeLoaded(name:String)
-{
-    switch (name)
-    {
+function onEditorTreeLoaded(name:String) {
+    switch (name) {
         case "Character Editor":
             DiscordUtil.changePresenceSince("ayo finding a character", null);
         case "Chart Editor":
@@ -47,10 +38,8 @@ function onEditorTreeLoaded(name:String)
     }
 }
 
-function onEditorLoaded(name:String, editingThing:String)
-{
-    switch (name)
-    {
+function onEditorLoaded(name:String, editingThing:String) {
+    switch (name) {
         case "Character Editor":
             DiscordUtil.changePresenceSince("i edit character", editingThing);
         case "Chart Editor":
