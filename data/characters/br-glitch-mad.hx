277817b4c8b9a42a5ab83aa29ab340f168a7c29e
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-20 20:30:23 -0400

    glitched br phase 2

---
+++ data/characters/br-glitch-mad.hx
@@ -0,0 +1,20 @@
+import flixel.addons.effects.FlxTrail;
+
+var self = this;
+var trail:FlxTrail;
+
+function postCreate() {
+    if (Options.week6PixelPerfect) this.pixelPerfectRender = this.pixelPerfectPosition = true;
+    
+    trail = new FlxTrail(self, null, 4, 24, 0.3, 0.069);
+}
+
+var toAdd:Bool = true; // Using this just to make sure
+
+function update(elpased) {
+    if (toAdd) {
+        toAdd = false;
+        PlayState.instance.insert(PlayState.instance.members.indexOf(self), trail);
+        disableScript();
+    }
+}
