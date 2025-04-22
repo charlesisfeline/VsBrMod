716260948e78cf670f9202fdf8a9c5ec4d013623
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-22 15:49:52 -0400

    smiler cutscene

--- songs/smiler/scripts/shaders.hx
+++ songs/smiler/scripts/shaders.hx
@@ -15,13 +15,13 @@ function postCreate() {
     trace("add vhs");
     vhs = new CustomShader("vhs");
     vhs.time = 0;
-    vhs.noiseIntensity = 0.003;
+    vhs.noiseIntensity = 0.002;
     vhs.colorOffsetIntensity = 0.1;
     if (Options.gameplayShaders) FlxG.camera.addShader(vhs);
     
     staticShader = new CustomShader("tvstatic");
     staticShader.time = 0;
-    staticShader.strength = 0.3;
+    staticShader.strength = 0.15;
     staticShader.speed = 20;
     if (Options.gameplayShaders) FlxG.camera.addShader(staticShader);
 }
